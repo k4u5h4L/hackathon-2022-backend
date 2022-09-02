5bca375e540c51623daabef514a65e3803cd7d46
k4u5h4L <kaushal.v.bhat@gmail.com>
2022-09-03 01:22:20 +0530

    CRUD added

--- asset_tracker/serializers.py
+++ asset_tracker/serializers.py
@@ -2,7 +2,7 @@ from django.db.models import fields
 from rest_framework import serializers
 
 from users.models import CustomUser, Profile
-from .models import Asset
+from .models import Asset, AssetAssigned, AssetFeedback, AssetRequested
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -16,7 +16,8 @@ class UserSerializer(serializers.ModelSerializer):
         model = CustomUser
 
         # exclude = ('password',)
-        fields = ['username', 'email', 'about', 'groups']
+        fields = ['username', 'email', 'about', 'groups', 'designation',
+                  'objects', 'is_staff', 'is_active', 'start_date', ]
         depth = 1
 
 
@@ -30,13 +31,68 @@ class AssetListSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class AssetCreateSerializer(serializers.ModelSerializer):
-    # created_by = serializers.PrimaryKeyRelatedField(
-    #     read_only=True, default=serializers.CurrentUserDefault())
-    # update_by = serializers.PrimaryKeyRelatedField(
-    #     read_only=True, default=serializers.CurrentUserDefault())
+class AssetAssignedListSerializer(serializers.ModelSerializer):
+    created_by = UserSerializer()
+    update_by = UserSerializer()
+    assigned_to = UserSerializer()
+    approved_by = UserSerializer()
+    assigned_asset = AssetListSerializer()
 
+    class Meta:
+        model = AssetAssigned
+        depth = 2
+        fields = '__all__'
+
+
+class AssetRequestedListSerializer(serializers.ModelSerializer):
+    created_by = UserSerializer()
+    update_by = UserSerializer()
+    requested_by = UserSerializer()
+    requested_to = UserSerializer()
+
+    class Meta:
+        model = AssetRequested
+        depth = 2
+        fields = '__all__'
+
+
+class AssetFeedbackListSerializer(serializers.ModelSerializer):
+    created_by = UserSerializer()
+    update_by = UserSerializer()
+    asset = AssetListSerializer()
+    feedback_by = UserSerializer()
+
+    class Meta:
+        model = AssetFeedback
+        depth = 2
+        fields = '__all__'
+
+
+class AssetCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Asset
         fields = ('name', 'serial_number', 'model', 'amount', 'category', 'category_type',
                   'purchase_date', 'manufactured_date')
+
+
+class AssetAssignedCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AssetAssigned
+        fields = ('assigned_date', 'asset_status',
+                  'assigned_to', 'approved_by')
+
+
+class AssetRequestedCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AssetRequested
+        fields = ('requested_date', 'requested_by',
+                  'requested_to', 'manager_approval', 'reason',
+                  'approval_status', 'request_needed', 'category',
+                  'category_type')
+
+
+class AssetFeedbackCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AssetFeedback
+        fields = ('feedback_by', 'feedback_date',
+                  'feedback', 'is_working', 'productivity_rating')
--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import CustomUserCreate, BlacklistTokenUpdateView
+from .views import CustomUserCreate, BlacklistTokenUpdateView, get_user
 
 app_name = 'users'
 
 urlpatterns = [
     path('register/', CustomUserCreate.as_view(), name='create-user'),
     path('logout/blacklist/', BlacklistTokenUpdateView.as_view(),
-         name='blacklist')
+         name='blacklist'),
+    path('', get_user, name='get-user')
 ]
